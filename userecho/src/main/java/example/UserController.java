c996c8825e2a9d58fbf46a5460d33a3fa01bb52c
dptorri <dptorri@gmail.com>
2021-11-22 18:49:25 +0100

    10. Create a class UserController which exposes /user endpoint (userecho)

---
+++ userecho/src/main/java/example/UserController.java
@@ -0,0 +1,21 @@
+package example;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Produces;
+import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.rules.SecurityRule;
+
+import java.security.Principal;
+
+@Controller("/user")
+public class UserController {
+
+    @Secured(SecurityRule.IS_AUTHENTICATED)
+    @Produces(MediaType.TEXT_PLAIN)
+    @Get
+    String index(Principal principal) {
+        return principal.getName();
+    }
+}
